64a86bea7f6ea48b8e61d2368278942e65706f32
Ульянов Сергей <82935685+uk8ahg@users.noreply.github.com>
2021-05-22 16:01:58 +0500

    Create 8-3sql_req.py

---
+++ 8-3sql_req.py
@@ -0,0 +1,22 @@
+import sqlite3
+conn = sqlite3.connect('database.sqlite')
+cursor = conn.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users
+(TELEGRAM_ID INTEGER,
+USER_NAME STRING,
+PHONE INTEGER,
+LANG INTEGER )
+''')
+reg_in_table = '''INSERT INTO Users VALUES ('{}','{}',0,0)
+'''
+update_lang_in_table = '''
+UPDATE Users
+SET LANG = '{}'
+WHERE TELEGRAM_ID = '{}'
+'''
+lang_in_table = '''
+SELECT LANG
+FROM Users
+WHERE TELEGRAM_ID = '{}'
+'''
