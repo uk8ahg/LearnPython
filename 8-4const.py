e3bbbef9dd6319ce9790f375d2a46286177e985f
Ульянов Сергей <82935685+uk8ahg@users.noreply.github.com>
2021-05-22 16:02:22 +0500

    Create 8-4const.py

---
+++ 8-4const.py
@@ -0,0 +1,24 @@
+TOKEN = '1799454556:AAFUQu-harYu7XL4y1fVOg1QeK92Uwpt1pk'
+GLOBAL_DCT = {1:['Assalom'],
+              2:['Привет']}
+
+DICT_QUEST={1:['Как твои дела?','Ishlaring qalay?'],
+          2:['Кто ты?','Sen kimsan?'],
+          3:['Как тебя зовут?','Isming nima?'],
+          4:['Расскажи о жизни, смерти и вообще','Hayot, o\'lim va umuman olganda bizga xabar bering'],
+          5:['Ты много умеешь?','Bilasizmi qancha?'],
+          6:['Кто я?','Kimman?'],
+          7:['Что ты умеешь?','Siz nima qila olasiz?'],
+          8:['Ты человек?','Siz odammisiz?'],
+          9:['Ты бот?','Siz botmisiz?'],
+          10:['Зачем?','Nima uchun?']}
+DICT_ANS={1:['Спасибо,хорошо','Rahmat yaxshi'],
+          2 : ['Я бот который умеет отвечать на вопросы','Men savollarga javob bera oladigan botman'],
+          3 : ['Я еще не заслужил имя :(((','Men hali nom topolmadim :((('],
+          4 : ['42','42'],
+          5 : ['Пока нет, но я стараюсь учиться','Hali emas, lekin men o\'rganishga harakat qilaman'],
+          6 : ['Человек, наверное...','Inson, ehtimol...'],
+          7 : ['Ну... Я могу отвечать на вопросы... На некоторые','Xo\'sh ... Men savollarga javob bera olaman ... ba\'zilari'],
+          8 : ['ПОКА нет)))','HALI emas'],
+          9 : ['ПОКА да','HA ha'],
+          10 : ['ПАТАМУШТА))))','MAN DEDIM)))))']}
