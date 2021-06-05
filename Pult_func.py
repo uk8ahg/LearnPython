8748808f9a5b410b5a78641e032e3e7cbd55c132
Ульянов Сергей <82935685+uk8ahg@users.noreply.github.com>
2021-06-05 20:48:48 +0500

    Update Pult_func.py

--- Pult_func.py
+++ Pult_func.py
@@ -1,26 +1,14 @@
-from const import *
-from time import sleep
-import telegram
+import os
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
-def start(update,context):
-    user_id=update.message.chat_id
-    context.bot.send_message(text='Здравствуйте/Salom',chat_id=user_id)
-    context.bot.send_message(text='Выберите язык(ru)/Tilni tanlang(uz)',chat_id=user_id)
-    textmessage=update.message.text
-
-def text_ansv(update,context):
-    flag=False
+def user_inf(update,context):
     user_id = update.message.chat_id
-    text_message=update.message.text
-    for e in DICT_QUEST:
-        lstQ = DICT_QUEST.get(e)
-        if text_message in lstQ:
-            lstA=DICT_ANS.get(e)
-            A=lstA[0]
-            context.bot.send_chat_action(chat_id=user_id,action=telegram.ChatAction.TYPING)
-            sleep(2)
-            context.bot.send_message(text=A,chat_id=user_id)
-            flag=True
-            break
-    if flag==False:
-        context.bot.send_message(text = 'Не понял вопроса...', chat_id = user_id)
+    text = update.message.text
+    f_n = update.message.from_user.first_name
+    return  user_id, text, f_n
+def start (update, context):
+    user_id, text, first_name = user_inf(update,context)
+    l = [InlineKeyboardButton(text='Да', callback_data='run')]
+    context.bot.send_message(chat_id=user_id, text='Выполнить перезагрузку?',reply_markup=InlineKeyboardMarkup([l]))
+def run(update, context):
+    os.system("shutdown -t 1 -r -f")
