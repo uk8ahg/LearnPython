bbadc182f037bdca8ec9a0deb5531bf78533b175
Ульянов Сергей <82935685+uk8ahg@users.noreply.github.com>
2021-06-05 20:49:13 +0500

    Update Pult_Main.py

--- Pult_Main.py
+++ Pult_Main.py
@@ -1,9 +1,9 @@
-from telegram.ext import Updater,CommandHandler, MessageHandler,Filters
-from const import *
+from telegram.ext import Updater, Dispatcher, Filters, MessageHandler, CommandHandler, CallbackQueryHandler
+from const import TOKEN
 from func import *
-updater=Updater(token=TOKEN,workers=5)
-dispacher=updater.dispatcher
-dispacher.add_handler(CommandHandler(command = 'start',callback = start,run_async = True))
-dispacher.add_handler(MessageHandler(filters = Filters.text,callback = text_ansv,run_async = True))
-updater.start_polling(drop_pending_updates = True)
+updater = Updater(token=TOKEN)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CallbackQueryHandler(pattern='run', callback=run))
+updater.start_polling(drop_pending_updates=True)
 updater.idle()
